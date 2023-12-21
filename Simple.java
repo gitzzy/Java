275eed199cf4aca8df6e4100e532c69e445b9ef9
gitzzy <tyagidevansh10@gmail.com>
2023-12-21 20:29:39 +0530

    New File

---
+++ Simple.java
@@ -0,0 +1,5 @@
+public class Simple {
+    public static void main(String[] args){
+        System.out.println("This is to demonstrate simple gihtub features and its effect on new file.");
+    }
+}
