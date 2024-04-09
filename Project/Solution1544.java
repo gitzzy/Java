8bb99d28f8cd246af4bfbfaf473ef246deefdc15
gitzzy <tyagidevansh10@gmail.com>
2024-04-09 19:41:21 +0530

    .

--- Project/Solution1544.java
+++ Project/Solution1544.java
@@ -1,8 +1,10 @@
 class Solution1544 {
     public static String makeGood(String s) {
         char[] ch = s.toCharArray();
-        for(int i=0; i<s.length()-1; i++){
-
+        
+        for(int i=0; i<ch.length-1; i++){
+            if(Math.abs(ch[i]-ch[i+1]) == 32){
+            }
         }
     }
     public static void main(String[] args){
